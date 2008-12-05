f4b0f4a18cd4a26413ae855c821fba30c7460284
benwarfield <benwarfield@4cb3fe20-bb1e-11dd-a80a-757f205f25d6>
2008-12-05 23:08:24 +0000

    Minor refactoring of Engine code, with new methods in datastructures to make it easier.

--- engine.lisp
+++ engine.lisp
@@ -57,17 +57,16 @@
 
 
 
-(defun Engine (rules WM &optional (rules-tried 0) (rule (car rules)) 
-                                  (LHS (get-LHS (car rules))) (RHS (get-RHS (car rules)))  ;create LHS and RHS on the go, using this part as an intial let
-                                  (new-rules (append (cdr rules) (list (car rules)))))     ;ready the new-rules order in case rule can not fire (put it at the end)
+(defun Engine (rules WM &optional (rules-failed 0) (rule (car rules)) 
+	                          (RHS (get-RHS (car rules)))  ;create  RHS on the go, using this part as an intial let
+                                  (new-rules (append (cdr rules) (list rule))))     ;ready the new-rules order in case rule can not fire (put it at the end)
   ;(print WM)  ;for debug purposes
   (cond ((null rules) WM)                                             ;if rules list is empty just return WM
-    ((>= rules-tried (length rules)) WM)                              ;if tried all the rules without any action result, return WM
-    ((equal LHS '(T)) (Do-Actions RHS '(T NIL) WM new-rules (1+ rules-tried)))  ;special case: if LHS is T do actions without match, lower priority to avoid looping on that rule
+    ((>= rules-failed (length rules)) WM)                              ;if tried all the rules without any action result, return WM
     (T (let* ((match-result (match-rule rule WM)))
          (if (null match-result)                                      ;if no match is found: 
-             (engine new-rules WM (1+ rules-tried))                   ;call engine again, but lower priority of the rule to lowest, increase rules-tried by 1.
-             (Do-Actions RHS match-result WM (append (list rule) (cdr rules)) 0))))))            ;otherwise, call do-actions to decide how to continue (set rules tried to zero)
+             (engine new-rules WM (1+ rules-failed))                   ;call engine again, but lower priority of the rule to lowest, increase rules-failed by 1.
+             (Do-Actions RHS match-result WM rules 0))))))            ;otherwise, call do-actions to decide how to continue (set rules tried to zero)
 
 
 
@@ -75,7 +74,7 @@
 
 
 ;;;
-;Do-Actions RHS match-result WM rules rules-tried &optional bindings facts command argument      [Function]
+;Do-Actions RHS match-result WM rules rules-failed &optional bindings facts command argument      [Function]
 ;
 ; Inputs:
 ;
@@ -83,7 +82,7 @@
 ;   2.match-result: <list>, return object of match-rule
 ;   3.WM: <list>, list of facts defined within our RPS syntax
 ;   4.rules: <list>, list of rules to be tried.used when calling engine (if no TERMINATE command is encountered)
-;   5.rules-tried: <positive integer> keeps how many rules could not fire
+;   5.rules-failed: <positive integer> keeps how many rules could not fire
 ;
 ;   optional:
 ;
@@ -104,27 +103,27 @@
 
 
 
-(defun Do-Actions (RHS match-result WM rules rules-tried &optional (bindings (car match-result))  (facts (cadr match-result))    ;initializations
+(defun Do-Actions (RHS match-result WM rules rules-failed &optional (bindings (car match-result))  (facts (cadr match-result))    ;initializations
                                                                    (command (caar RHS))           (argument (cadar RHS)))
-  (cond ((null RHS) (Engine rules WM rules-tried))   ;if RHS is null (all actions done), recall engine.
+  (cond ((null RHS) (Engine rules WM rules-failed))   ;if RHS is null (all actions done), recall engine.
     ((eql command 'TERMINATE) WM)                    ;TERMINATE - > stop execution,return WM
     ((eql command 'ADD) (Do-Actions (cdr RHS)        ;ADD - > substitue bindings, add to WM, recall do-actions
                                     match-result 
                                     (WMadd (car (substitute-action (list argument) bindings)) WM) 
                                     rules 
-                                    rules-tried))  
+                                    rules-failed))  
     ((eql command 'REMOVE) (Do-Actions (cdr RHS)     ;REMOVE - > delete from WM, recall do-actions
                                        match-result 
                                        (WMDelete (car (subseq facts (1- argument) argument)) WM) 
                                        rules 
-                                       rules-tried))            
+                                       rules-failed))            
     (T (error "Unrecognized Command"))))             ;undefined command error
 
 
 
 
 ;;Generelization functions;;
-;
+;  (now just shims over to generic methods in datastructures.lisp)
 ;
 (defun get-LHS (rule)
   (pattern-list rule))
@@ -134,14 +133,12 @@
 
 
 (defun WMadd (fact WM)
-    (append (list fact) WM))
+   (add-fact wm fact)
+)
 
 (defun WMDelete (fact WM)
-  (let* ((suffix (member fact WM :test #'Equal))
-         (prefix (subseq WM 0 (- (length WM) (length suffix)))))
-   (if (not (eq suffix nil))
-       (append prefix (cdr suffix))
-       WM)))
+  (delete-fact wm fact)
+)
 
 
 ;;;;
