1c1a41818190b9126f5346ddb0679d717b5d3db5
benwarfield <benwarfield@4cb3fe20-bb1e-11dd-a80a-757f205f25d6>
2008-12-06 22:15:11 +0000

    Added top-level engine command and a test/demo for it.

---
+++ trunk/TEST.lisp
@@ -0,0 +1,11 @@
+(require "movie-expert.lisp")
+
+(recommend-movies 
+ '(
+   "Goodfellas\ \(1990\)"
+   "Rear\ Window\ \(1954\)"
+   "Raiders\ of\ the\ Lost\ Ark\ \(1981\)"
+   "Psycho\ \(1960\)" 
+   "Memento\ \(2000\)" 
+   )
+)
